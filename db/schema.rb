81c98a410eadf8de93e030c31d715ddd54020792
Kyle Lindsay <kyle.lindsay333@gmail.com>
2024-10-29 09:44:27 -0700

    add sales feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_27_155459) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_29_031005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_27_155459) do
     t.datetime "updated_at", null: false
     t.bigint "category_id"
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "sales", force: :cascade do |t|
+    t.string "name"
+    t.integer "percent_off"
+    t.date "starts_on"
+    t.date "ends_on"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
